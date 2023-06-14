48f1438ec0f896033441e477c1a81cd8ad3afce4
mclean1 <mclean1@cs.dal.ca>
2023-06-14 19:48:13 -0300

    Got server login with rachel.whc.ca working!

--- php/server_login.php
+++ php/server_login.php
@@ -1,9 +1,9 @@
 <?php 
     //Sign in and page array variables
-    $host = 'localhost';
-    $database = 'csci4140_assn1_771';
-    $username = 'root';
-    $password = 'root';
+    $host = 'rachel.whc.ca';
+    $database = 'lvrukr54_csci4140_assn2';
+    $username = 'lvrukr54_admin';
+    $password = 'js?MFDwx*Ap?';
     //Connects to database
     $conn = new mysqli($host, $username, $password, $database);
     if ($conn->connect_error) {
